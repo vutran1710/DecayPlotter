4cee565826ea1fc6d09bbf24e7b56827b106aeb2
vutr <me@vutr.io>
2020-03-06 17:35:26 +0700

    naming

--- src/Functions/index.js
+++ src/Functions/index.js
@@ -13,11 +13,11 @@ export const score_calc = (origin, r, t, half_time) => {
 }
 
 
-export const S1T = (origin, t, half_time) => origin * Math.pow(1/half_time, t)
+export const S1T = (origin, t, steepness) => origin * Math.pow(1/steepness, t)
 
 export const S2T = (upperBound, x, Breakpoint, steepness = 1.05) => {
   // https://towardsdatascience.com/transforming-skewed-data-73da4c2d0d16
   // if (x === 0) return 0
-  const base = 1 + Math.pow(steepness, -(x - Breakpoint))
+  const base = 1 + Math.pow(steepness, Breakpoint - x)
   return upperBound / base
 }
